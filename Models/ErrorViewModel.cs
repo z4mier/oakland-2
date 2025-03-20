4a86a7ea7d2c43b7484c5486ee334eac54437f25
Zaira Mae Mier <z4mier@gmail.com>
2025-03-20 17:32:21 +0800

    Front Page

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Oakland_Homeowners.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
